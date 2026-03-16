676f06a0fa74c4f980d2dfdb5fa3924d7ad31262
repagithhdo <taylorserren02@gmail.com>
2026-03-16 02:21:03 -0400

    Simplify script loading by removing task.spawn
    Removed redundant task.spawn calls for loading scripts.

--- script.lua
+++ script.lua
@@ -1,7 +1,1 @@
-task.spawn(function()
- loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ef18981bd4d52c2ae04feca4bf5bd94a.lua"))()
-end)
-
-task.spawn(function()
-   loadstring(game:HttpGet("https://pastebin.com/raw/8CrdCWVC"))()
-end)
+loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ef18981bd4d52c2ae04feca4bf5bd94a.lua"))()
